f608172473a1ff4d4b8e196d7249a2dce9a87291
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-11-16 20:58:01 +0100

    2d array print (board creation)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
 use std::io::stdin;
-
-fn take_input(input: &str) -> String {
-    println!("{input}");
-    let mut buf = String::new();
-    stdin()
-        .read_line(&mut buf)
-        .expect("Failed to read buffer in input");
-    buf = buf.trim().to_lowercase();
-    return buf;
-}
 fn main() {
-    let board_size = take_input("what is your amongus");
-    board_size = board_size.parse::<u8>();
-    println!("{boarz_size}")
+    let arr = [[Square::Empty; 10]; 10];
+    println!("{}", board_to_string(&arr));
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).expect("Failed to read input");
+}
+
+#[derive(Clone, Copy)]
+enum Square {
+    Empty,
+    Miss,
+    Hit,
+    Ship,
+}
+
+use std::fmt;
+impl fmt::Display for Square {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Square::Empty => " ",
+                Square::Miss => "O",
+                Square::Hit => "X",
+                Square::Ship => "#",
+            }
+        )
+    }
+}
+
+fn board_to_string(board: &[[Square; 10]; 10]) -> String {
+    use std::fmt::Write;
+    let mut result = String::new();
+    write!(result, "+ A + B + C + D + E + F + G + H + I + J +\n",).unwrap();
+    for row in board {
+        write!(result, "+---+---+---+---+---+---+---+---+---+---+\n",).unwrap();
+        for square in row {
+            write!(result, "| {} ", square).unwrap();
+        }
+        write!(result, "|\n").unwrap();
+    }
+    write!(result, "+---+---+---+---+---+---+---+---+---+---+",).unwrap();
+    result
 }
